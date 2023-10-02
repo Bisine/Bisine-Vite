80095c002565c727b30c939aa6132832eeae2b57
aswin-codes <codicon.co@gmail.com>
2023-10-02 14:04:42 +0530

    Error handling

--- src/screens/profileCreate/ProfileCreation.jsx
+++ src/screens/profileCreate/ProfileCreation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFullName, setPhoneNumber } from "../../redux/features/user";
 import { useNavigate } from "react-router-dom";
@@ -7,35 +7,76 @@ import axiosInstance from "../../axios";
 const ProfileCreation = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const email = useSelector(e => e.user.email)
+
+  const email = useSelector((e) => e.user.email);
   const phoneNumber = useSelector((event) => event.user.phoneNumber);
   const fullName = useSelector((event) => event.user.fullName);
 
-  const onSubmit = (e) => {    
-    e.preventDefault();
-    axiosInstance.post('user/register/',{
-      email: localStorage.getItem('email'),
-      user_name: fullName,
-      phone_number: "+91"+phoneNumber
-    }).then(res => {
-      if (res.status == 201){
-        axiosInstance.post('user/token/',{
-          email: email
-        }).then(res => {
-          if (res.status == 200){
-            localStorage.setItem('refresh', res.data.refresh);
-            localStorage.setItem('access', res.data.access)
-            navigate('/')
-          }
-        })
-      } 
-    })
+  const [isChecked, setIsChecked] = useState(false);
+  const [isError, setIsError] = useState(null);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    setIsError(null)
+    setErrorMsg('')
+  }, [isChecked, phoneNumber, fullName]);
+
+  function isValidPhoneNumber(phoneNumber) {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phoneNumber);
   }
+
+  const onSubmit = (e) => {
+    
+    e.preventDefault();
+
+    if (fullName == ''){
+      setErrorMsg("Kindly, enter your user name");
+      setIsError(true)
+    }
+
+    else if (phoneNumber == '' || !isValidPhoneNumber(phoneNumber)){
+      setErrorMsg('Kindly, enter your phone number');
+      setIsError(true)
+    }
+
+    else if (isChecked){
+      setIsError(null)
+      axiosInstance
+      .post("user/register/", {
+        email: localStorage.getItem("email"),
+        user_name: fullName,
+        phone_number: "+91" + phoneNumber,
+      })
+      .then((res) => {
+        if (res.status == 201) {
+          axiosInstance
+            .post("user/token/", {
+              email: email,
+            })
+            .then((res) => {
+              if (res.status == 200) {
+                localStorage.setItem("refresh", res.data.refresh);
+                localStorage.setItem("access", res.data.access);
+                navigate("/");
+              }
+            });
+        }
+      });
+    } else {
+      setErrorMsg('To create an account, you should accept terms and conditions');
+      setIsError(true);
+      
+    }
+  };
 
   return (
     <div className="flex-1 flex flex-col justify-center items-center text-black gap-5">
       <h1 className="font-semibold text-2xl">Register your account</h1>
-      <form onSubmit={(e) => onSubmit(e)} className="flex flex-col items-center gap-4 mt-10">
+      <form
+        onSubmit={(e) => onSubmit(e)}
+        className="flex flex-col items-center gap-4 mt-10"
+      >
         <div>
           <p className="font-semibold text-md">Full Name</p>
           <input
@@ -57,7 +98,12 @@ const ProfileCreation = () => {
           />
         </div>
         <div className="flex gap-5 items-center mt-16 w-60 lg:w-96">
-          <input type="checkbox" className="bg-blue-50"/>
+          <input
+            value={isChecked}
+            onChange={() => setIsChecked((prev) => !prev)}
+            type="checkbox"
+            className="bg-blue-50"
+          />
           <label className="text-xs">
             By creating an account, you are agreeing to our{" "}
             <span className="underline text-blue-700">
@@ -65,8 +111,33 @@ const ProfileCreation = () => {
             </span>
           </label>
         </div>
+        {
+          isError && 
+          <div className="bg-red-200 pl-2 pr-4 gap-2 w-60 lg:w-96 text-xs py-2 rounded-lg flex text-red-700">
+          <button className="h-8 w-8 " onClick={()=> setIsError(null)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5 text-red-800 object-contain"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+          </button>
 
-        <button type="submit" className="rounded-lg bg-blue-500 py-2 w-60 lg:w-96 mt-2">
+          <p>{errorMsg}</p>
+        </div>
+        }
+        <button
+          type="submit"
+          className="rounded-lg bg-blue-500 py-2 w-60 lg:w-96 mt-2"
+        >
           Create Account
         </button>
       </form>
