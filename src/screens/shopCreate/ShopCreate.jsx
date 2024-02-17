c3cafce8d778e4b525b0c3aaa0d9d1cb47c9ef5a
aswin-codes <codicon.co@gmail.com>
2024-02-17 19:39:29 +0530

    Environment varibles not working when deployed, hence hardcoding it api

--- src/screens/shopCreate/ShopCreate.jsx
+++ src/screens/shopCreate/ShopCreate.jsx
@@ -77,7 +77,7 @@ const ShopCreate = () => {
         try {
           
           const response = await axios.post(
-            `${import.meta.env.VITE_API_URL}/shop/checkUniqueId`,
+            `https:/bisinenode.vercel.app/api/shop/checkUniqueId`,
             {
               unique_id: shopId,
             }
@@ -155,7 +155,7 @@ const ShopCreate = () => {
       try {
         const accessToken = localStorage.getItem("access_token");
         const response = await axios.post(
-          `${import.meta.env.VITE_API_URL}/shop/create`,
+          `https:/bisinenode.vercel.app/api/shop/create`,
           shopData,
           {
             headers: {
